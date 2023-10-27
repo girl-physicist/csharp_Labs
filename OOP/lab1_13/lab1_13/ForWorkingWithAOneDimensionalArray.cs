127469f134efd02b2e052ee88ea0a3e9c478fa73
snapp <3gotosnapp@gmail.com>
2023-10-27 16:14:44 +0300

    lab1_v13

---
+++ OOP/lab1_13/lab1_13/ForWorkingWithAOneDimensionalArray.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace lab1_13
+{
+    public class ForWorkingWithAOneDimensionalArray
+    {
+        private int[] array;
+        public uint size { get; private set; }
+
+        public ForWorkingWithAOneDimensionalArray(uint arrayLength, string massiv)
+        {
+            size = arrayLength;
+            array = new int[arrayLength];
+            string[] separators = { ":"};
+            string[] split = massiv.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                try
+                {
+                    array[i] = int.Parse(split[i]);
+                }
+                catch 
+                {
+                  
+                }
+            }
+        }
+
+        public int FindSumOfNotNegativeElementThatIsLeftFirstNotNegativeElement()
+        {
+            var sum = 0;
+
+            foreach (var element in array)
+            {
+                if (element > 0)
+                {
+                    break;
+                   
+                } 
+                sum += element;
+            }
+
+            return sum;
+        }
+        
+    }
+}
---
+++ OOP/lab1_13/lab1_13/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace lab1_13
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var classForWorkingWithAOneDimensionalArray = new ForWorkingWithAOneDimensionalArray(5, ":10:12:14");
+            Console.Write(classForWorkingWithAOneDimensionalArray.FindSumOfNotNegativeElementThatIsLeftFirstNotNegativeElement());
+            Console.WriteLine(classForWorkingWithAOneDimensionalArray.size);
+        }
+    }
+}
